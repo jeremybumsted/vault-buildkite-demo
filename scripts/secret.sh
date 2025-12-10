c2d5833fe2fb93677cd1757beaa04c1438223dd3
Jeremy Bumsted <jeremy@buildkite.com>
2025-12-10 14:44:47 -0700

    adds a multiline secret for testing purposes

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "Artifactory user is: ${artifactory_user}"
+echo "Multiline secret is: ${multiline_secret}"
--- scripts/setup-vault.sh
+++ scripts/setup-vault.sh
@@ -9,7 +9,6 @@
 # for the cli commands
 set -euo pipefail
 
-
 export VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID:-"buildkite-is-cool"}"
 export VAULT_ADDRESS="http://127.0.0.1:8220"
 
@@ -20,15 +19,19 @@ vault secrets enable kv
 
 vault kv put secret/buildkite/vault-buildkite-demo/env some_value="alpacas" && sleep 1
 vault kv put kv/buildkite/vault-buildkite-demo/env artifactory_user="test" && sleep 1
+vault kv put kv/buildkite/vault-buildkite-demo/env multiline_secret="something or other \
+    comment" another="more \
+    info" date="2020482983" \
+  end="finished" && sleep 1
 
 # enable approle authentication
 vault auth enable approle && sleep 1
 vault auth enable jwt && sleep 1
 
 vault write auth/jwt/config \
-    oidc_discovery_url="https://agent.buildkite.com" \
-    oidc_client_id="" \
-    oidc_client_secret=""
+  oidc_discovery_url="https://agent.buildkite.com" \
+  oidc_client_id="" \
+  oidc_client_secret=""
 
 # create the policy that will be scoped to the token requested by the agent role
 cat ./config/agent-policy.hcl | vault policy write buildkite -
@@ -38,13 +41,12 @@ sleep 1
 # Create our agent roles, one for approle and another for jwt and attach the agent policies created earlier
 vault write auth/approle/role/buildkite token_policies="buildkite" token_ttl=1h token_max_ttl=4h && sleep 1
 vault write auth/jwt/role/buildkite \
-    role_type="jwt" \
-    allowed_redirect_uris="http://localhost:8250/oidc/callback" \
-    bound_audiences="https://buildkite.com/jeremy-test" \
-    user_claim="job_id" \
-    policies=buildkite \
-    ttl=1h
-
+  role_type="jwt" \
+  allowed_redirect_uris="http://localhost:8250/oidc/callback" \
+  bound_audiences="https://buildkite.com/jeremy-test" \
+  user_claim="job_id" \
+  policies=buildkite \
+  ttl=1h
 
 # Now we can collect our ROLE_ID and SECRET_ID
 echo "~~~ Successfully configured Vault at ${VAULT_ADDRESS}"
